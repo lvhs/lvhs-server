19bc4d438985602ce6af510411bbfe1ce7e447e0
Masayuki Uehara <munky69rock@gmail.com>
2014-10-21 22:52:04 +0900

    Add logic to check uiid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,28 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_uiid
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
+
+  def check_uiid
+    if Rails.env.production? && get_uiid == nil
+      render text: 'Error', status: 403
+    end
+  end
+
+  def get_uiid
+    if session.has_key?(:uiid)
+      logger.info 'retrieve uiid from session'
+      session[:uiid]
+    else
+      logger.info 'set uiid from header'
+      uiid = request.headers['x-uiid']
+      session[:uiid] = uiid
+      uiid
+    end
+  end
 
   def authenticate_admin_user!
     authenticate_staff!
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 Lvhs::Application.config.session_store(:cookie_store,
                                        key: Rails.env.production? ? '_lvhs_session' : (ENV['SESSION_KEY'] || "_lvhs_session_#{Rails.env}"),
-                                       domain: :all
+                                       domain: :all,
+                                       expire_after: 1.year
                                       )
