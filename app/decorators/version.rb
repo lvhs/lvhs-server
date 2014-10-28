cd3a4123d17183d2ec7d4620b32be5cceb8aa461
Masayuki Uehara <munky69rock@gmail.com>
2014-10-28 18:26:37 +0900

    Added nokogiri

---
+++ app/decorators/version.rb
@@ -0,0 +1,17 @@
+class Version
+  attr_reader(:major, :minor, :patch)
+
+  def initialize (version)
+    @major, @minor, @patch =
+      version.split('.').map(&:to_i)
+  end
+
+  def <=> (other)
+    return nil unless other.is_a?(Version)
+
+    [ major <=> other.major,
+      minor <=> other.minor,
+      patch <=> other.patch,
+    ].detect {|n| !n.zero?} || 0
+  end
+end
