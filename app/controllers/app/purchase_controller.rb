4e26b3c2b9b476916d31eb7fee846548e51c748e
Masayuki Uehara <munky69rock@gmail.com>
2014-11-11 19:01:12 +0900

    [from now] 2014/11/11 19:01:12
     app/controllers/app/purchase_controller.rb | 8 +++++---
     1 file changed, 5 insertions(+), 3 deletions(-)

--- app/controllers/app/purchase_controller.rb
+++ app/controllers/app/purchase_controller.rb
@@ -1,14 +1,16 @@
 class App::PurchaseController < App::BaseController
+  protect_from_forgery :except => :create
+
   def create
-    @item = Item.find(item_params)
+    @item = Item.find_by(item_params)
     render_error if @item.nil? || @device.nil?
-    PurchasedItem.create! key: @device.key, id: @item.id
+    PurchasedItem.create! key: @device.key, item_id: @item.id
     render json: { status: 'ok' }, status: 200
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:id)
+    params.permit(:id)
   end
 end
