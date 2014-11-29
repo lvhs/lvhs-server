6ca181eeb3307523ea17e26075dbf17dcefc721d
Masayuki Uehara <munky69rock@gmail.com>
2014-11-29 16:03:16 +0900

    Restrict admin access

--- lib/active_admin/domain_restriction.rb
+++ lib/active_admin/domain_restriction.rb
@@ -3,14 +3,14 @@ module ActiveAdmin
     extend ActiveSupport::Concern
 
     included do
-      before_filter :restrict_to_ope_domain
+      prepend_before_filter :restrict_to_ope_domain
     end
 
     private
 
     def restrict_to_ope_domain
-      if Rails.env.production? && request.domain != 'ope.lvhs.jp'
-        render 404
+      if Rails.env.production? && request.subdomain != 'ope'
+        render_404
       end
     end
   end
