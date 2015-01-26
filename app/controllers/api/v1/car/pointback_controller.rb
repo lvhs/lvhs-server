f32b20be361d92fc6f5776f0b546f84efbd295dc
Masayuki Uehara <munky69rock@gmail.com>
2015-01-26 18:31:06 +0900

    Fixed error

--- app/controllers/api/v1/car/pointback_controller.rb
+++ app/controllers/api/v1/car/pointback_controller.rb
@@ -4,10 +4,11 @@ class Api::V1::Car::PointbackController < Api::V1::ApiController
     remote_ip = request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
     if Settings.car.ip.include? remote_ip
       p = pointback_params
-      p[:id], p[:item_id] = p[:id].split(':')
+      id, p[:item_id] = p[:id].split(':')
       p[:cname].encode!('UTF-8', 'Shift_JIS')
+      p.delete(:id)
       RewardHistory.create! p
-      PurchasedItem.create! key: Device.find(p[:id]).key, item_id: p[:item_id]
+      PurchasedItem.create! key: Device.find(id).key, item_id: p[:item_id]
       render text: 'OK', status: 200
     else
       redirect_to app_car_error_index_path
