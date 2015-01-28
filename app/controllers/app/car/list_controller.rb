8a6c96ce1adfc32ac0571db68162fc9cac6d75b2
Masayuki Uehara <munky69rock@gmail.com>
2015-01-28 18:23:02 +0900

    Fixed a bug

--- app/controllers/app/car/list_controller.rb
+++ app/controllers/app/car/list_controller.rb
@@ -4,10 +4,9 @@ class App::Car::ListController < App::BaseController
     # FIXME
     if Rails.env.development?
       @device ||= Device.find_by(key: '165F5CAC-1469-45DC-8CFB-16CAF36D3248')
-      uiid = get_uiid || '165F5CAC-1469-45DC-8CFB-16CAF36D3248'
-      id = Device.find_by(key: uiid).id
+      _uiid = uiid || '165F5CAC-1469-45DC-8CFB-16CAF36D3248'
+      id = Device.find_by(key: _uiid).id
     else
-      uiid = get_uiid
       id = Device.find_by(key: uiid).id
     end
     iid = list_params[:iid]
