1b2f3aec61ab406f0cbdb0cc111c4ccf88b2446f
Masayuki Uehara <munky69rock@gmail.com>
2015-05-20 16:21:02 +0900

    Fixed a fatal bug

--- app/controllers/app/purchase_controller.rb
+++ app/controllers/app/purchase_controller.rb
@@ -3,17 +3,9 @@ class App::PurchaseController < App::BaseController
 
   def create
     p = item_params
-    logger.info("iid: #{p[:iid]}")
-    logger.info("pid: #{p[:pid]}")
     iid = p[:iid] || ProductManager.iid(p[:pid])
-    logger.info("iid!: #{p[:pid]}")
     @item = Item.find_by(id: iid)
-    logger.info("item?: #{@item.nil?}")
-    logger.info("device?: #{@device.nil?}")
-    render_error if @item.nil? || @device.nil?
-    logger.info("not error #{@device.key} #{@item.id}")
     PurchasedItem.create! key: @device.key, item_id: @item.id
-    logger.info("created!")
     render json: { status: 'ok' }, status: 200
   end
 
