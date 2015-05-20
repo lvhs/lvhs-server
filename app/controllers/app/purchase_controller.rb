489b084eb453f0a86c00f9e05cf8155605fbe31e
Masayuki Uehara <munky69rock@gmail.com>
2015-05-20 16:12:22 +0900

    Added debug code

--- app/controllers/app/purchase_controller.rb
+++ app/controllers/app/purchase_controller.rb
@@ -3,8 +3,13 @@ class App::PurchaseController < App::BaseController
 
   def create
     p = item_params
+    logger.info("iid: #{p[:iid]}")
+    logger.info("pid: #{p[:pid]}")
     iid = p[:iid] || ProductManager.iid(p[:pid])
+    logger.info("iid!: #{p[:pid]}")
     @item = Item.find_by(id: iid)
+    logger.info("item?: #{@item.nil?}")
+    logger.info("device?: #{@device.nil?}")
     render_error if @item.nil? || @device.nil?
     PurchasedItem.create! key: @device.key, item_id: @item.id
     render json: { status: 'ok' }, status: 200
