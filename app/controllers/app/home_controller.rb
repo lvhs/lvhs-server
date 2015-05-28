794429ba2d6540012bacd0c47dbdbcabd650eefc
Masayuki Uehara <munky69rock@gmail.com>
2015-05-29 01:54:27 +0900

    Speed up top page

--- app/controllers/app/home_controller.rb
+++ app/controllers/app/home_controller.rb
@@ -1,5 +1,12 @@
 class App::HomeController < App::BaseController
   def index
-    @artists = Artist.all
+    first_view_items = 3
+    if params[:rest]
+      @artists = Artist.available[first_view_items..-1]
+      @rest = true
+      render partial: 'artist_list', layout: false, locals: { artists: @artists, rest: @rest }
+    else
+      @artists = Artist.available.take(first_view_items)
+    end
   end
 end
