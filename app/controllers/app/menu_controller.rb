d02f4ec6e1ec1e706fdd50ed00311e8db46c1690
Masayuki Uehara <munky69rock@gmail.com>
2015-06-17 03:14:55 +0900

    Added menu items

--- app/controllers/app/menu_controller.rb
+++ app/controllers/app/menu_controller.rb
@@ -2,6 +2,16 @@ class App::MenuController < App::BaseController
   def index
     render json: [
       {
+        title: "掲示板",
+        type: "url",
+        data: "http://stage.lvhs.jp/app/events"
+      },
+      {
+        title: "プロフィール",
+        type: "url",
+        data: "http://stage.lvhs.jp/app/profile"
+      },
+      {
         title: "シェア",
         type: "share"
       },
