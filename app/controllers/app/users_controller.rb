9800a22c085f7aeae3870705bcf50f93c6f94ddb
Masayuki Uehara <munky69rock@gmail.com>
2015-06-22 00:35:03 +0900

    Enabled to edit user profile

--- app/controllers/app/users_controller.rb
+++ app/controllers/app/users_controller.rb
@@ -8,6 +8,12 @@ class App::UsersController < App::BaseController
   end
 
   def update
-
+    @user = User.find(params[:id])
+    user_param = params.require(:user).permit(:name, :description)
+    if @user.update! user_param
+      redirect_to app_user_path(@user)
+    else
+      redirect_to edit_app_user_path(@user)
+    end
   end
 end
