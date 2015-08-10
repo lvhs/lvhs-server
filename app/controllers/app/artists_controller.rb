87a6ce1e3bfb036e4d7f4e9804f83f28b95b26c0
Masayuki Uehara <munky69rock@gmail.com>
2015-08-10 12:53:30 +0900

    Adjusted order

--- app/controllers/app/artists_controller.rb
+++ app/controllers/app/artists_controller.rb
@@ -2,10 +2,16 @@ class App::ArtistsController < App::BaseController
   def show
     p = artist_params
     @artist = Artist.find(p[:id])
+    # TODO:
     @price = {}.tap do |price|
       @artist.item.available.each do |item|
         price[item.id] = item.id == 451 ? 240 : 120
       end
+    end
+    if @artist.id == 1
+      @items = @artist.item.available.to_a.sort_by!{ |item| item.id == 451 ? 462 : item.id }.reverse
+    else
+      @items = @artist.item.available.reverse
     end
     @player = p[:player]
   end
