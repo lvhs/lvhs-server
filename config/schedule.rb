8b8739284cc87f0aed9abbcac62f9b2b9722c4c6
Masayuki Uehara <uehara.masayuki@dena.jp>
2015-08-24 00:56:18 +0900

    Fixed some issue

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+job_type :rbenv_rake, %q!eval "$(rbenv init -)"; cd :path && :environment_variable=:environment :bundle_command rake :task --silent :output!
+
 every 1.minute do
-  rake 'vimeo:publish'
+  rbenv_rake 'vimeo:publish'
 end
