5f6690df31d04affcf2ef615b2f69d6a1fc5c490
Masayuki Uehara <munky69rock@gmail.com>
2015-11-04 23:35:52 +0900

    Maybe this will fix problem

--- app/models/ca/reward/response.rb
+++ app/models/ca/reward/response.rb
@@ -20,8 +20,6 @@ class CA
             CA::Reward::Ad.new(ad_data).tap { |ad| convert_parameters(ad, user_id, enc_user_id) }
           rescue => e
             puts "CA::Reward::Ad error!:#{i}: #{e}"
-            print i
-            puts ad_data
             nil
           end
         end.compact
