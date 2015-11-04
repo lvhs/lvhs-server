0a08fb8e87de3761447e7042e1b34bca5e90d744
Masayuki Uehara <munky69rock@gmail.com>
2015-11-04 23:51:58 +0900

    Splat

--- app/models/ca/reward/cv_point.rb
+++ app/models/ca/reward/cv_point.rb
@@ -5,7 +5,8 @@ class CA
                   :price, :requisite_id
 
       def initialize(data = {})
-        Hash[data].each_pair do |k, v|
+        data = data.is_a?(Array) ? Hash[*data] : data
+        data.each_pair do |k, v|
           instance_variable_set("@#{k}", v)
         end
       end
