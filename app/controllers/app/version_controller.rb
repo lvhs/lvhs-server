e9a6a03f560696e79ca72440b25dfda0efa03ccc
Masayuki Uehara <munky69rock@gmail.com>
2015-12-01 12:50:35 +0900

    Force update (v0.0.6)

--- app/controllers/app/version_controller.rb
+++ app/controllers/app/version_controller.rb
@@ -1,7 +1,7 @@
 class App::VersionController < App::BaseController
   def index
     render json: {
-      required_version: '0.0.5', #latest_bundle_version.to_s,
+      required_version: latest_bundle_version.to_s,
       type: "force",
       update_url: Settings.app_store_url
     }
