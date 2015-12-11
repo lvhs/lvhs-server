b0f48e5a3913d28e84ae3dfeb8707b31a306f5ba
Masayuki Uehara <munky69rock@gmail.com>
2015-12-11 18:54:29 +0900

    Removed constraints

--- config/routes.rb
+++ config/routes.rb
@@ -7,63 +7,59 @@ Lvhs::Application.routes.draw do
   resources :terms, only: [:index], controller: 'teaser/terms'
   resources :privacy, only: [:index], controller: 'teaser/privacy'
 
-  constraints Routes::Constraints::Subdomain::API do
-    namespace :api do
-      namespace :v1 do
-        resources :home, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :home, only: [:index]
 
-        resources :artists, only: [:show]
+      resources :artists, only: [:show]
 
-        # CA Reward
-        namespace :car do
-          resources :pointback, only: [:index]
-          resources :list,      only: [:index]
-          resources :error,     only: [:index]
-        end
+      # CA Reward
+      namespace :car do
+        resources :pointback, only: [:index]
+        resources :list,      only: [:index]
+        resources :error,     only: [:index]
       end
     end
   end
 
-  constraints Routes::Constraints::Subdomain::APP do
-    namespace :app do
-      root controller: :home, action: :index
+  namespace :app do
+    root controller: :home, action: :index
 
-      resources :menu, only: [:index]
+    resources :menu, only: [:index]
 
-      resources :version, only: [:index]
+    resources :version, only: [:index]
 
-      resources :artists, only: [:index, :show]
+    resources :artists, only: [:index, :show]
 
-      resources :products, only: [:index]
+    resources :products, only: [:index]
 
-      resources :purchase, only: [:create]
+    resources :purchase, only: [:create]
 
-      resources :users, only: [:new, :create, :show, :edit, :update] do
-        member do
-          get   'image', to: 'users/image#edit'
-          patch 'image', to: 'users/image#update'
-        end
+    resources :users, only: [:new, :create, :show, :edit, :update] do
+      member do
+        get   'image', to: 'users/image#edit'
+        patch 'image', to: 'users/image#update'
       end
+    end
 
-      resources :profile, only: [:index]
+    resources :profile, only: [:index]
 
-      resources :events do
-        resources :comments, module: :events
-        resources :entry, module: :events, only: [:create, :destroy]
-      end
+    resources :events do
+      resources :comments, module: :events
+      resources :entry, module: :events, only: [:create, :destroy]
+    end
 
-      # CA Reward
-      namespace :car do
-        resources :list, only: [:index]
-        resources :error, only: [:index]
-      end
+    # CA Reward
+    namespace :car do
+      resources :list, only: [:index]
+      resources :error, only: [:index]
+    end
 
-      resources :videos do
-        get ':id', to: 'videos#show'
-        collection do
-          put :title, to: 'videos#title'
-          get :callback, to: 'videos#callback'
-        end
+    resources :videos do
+      get ':id', to: 'videos#show'
+      collection do
+        put :title, to: 'videos#title'
+        get :callback, to: 'videos#callback'
       end
     end
   end
